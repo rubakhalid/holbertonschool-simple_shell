4bc877bbb544b4b487e2615b3d86bacc4440349a
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-04-14 11:40:58 +0000

    Fix: support arguments and C89 compatibility

--- exec_cmd.c
+++ exec_cmd.c
@@ -3,20 +3,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 /**
- * execute_command - Executes a command with no arguments
- * @line: Command line input (single word)
+ * execute_command - Executes a command with optional arguments
+ * @line: Command line input
  */
 void execute_command(char *line)
 {
 	pid_t pid;
-	char *argv[] = {line, NULL};
+	char *argv[64];
+	int i = 0;
+	char *token;
+
+	token = strtok(line, " 	");
+	while (token != NULL && i < 63)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " 	");
+	}
+	argv[i] = NULL;
+
+	if (argv[0] == NULL)
+		return;
 
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(line, argv, environ) == -1)
+		if (execve(argv[0], argv, environ) == -1)
 			perror("hsh");
 		exit(1);
 	}
--- main.c
+++ main.c
@@ -3,24 +3,27 @@
 
 /**
  * main - Entry point of the shell
+ *
  * Return: Always 0
  */
 int main(void)
 {
 	char *line = NULL;
+	int only_spaces;
+	int i;
 
 	while (1)
 	{
 		line = read_line();
-		if (line == NULL)
+		if (line == NULL) /* Handle Ctrl+D */
 		{
 			write(1, "\n", 1);
 			break;
 		}
 
-		/* تجاهل السطور الفارغة */
-		int only_spaces = 1;
-		for (int i = 0; line[i]; i++)
+		/* Check for empty or whitespace-only input */
+		only_spaces = 1;
+		for (i = 0; line[i]; i++)
 		{
 			if (line[i] != ' ' && line[i] != '\t')
 			{
@@ -28,6 +31,7 @@ int main(void)
 				break;
 			}
 		}
+
 		if (only_spaces)
 		{
 			free(line);
