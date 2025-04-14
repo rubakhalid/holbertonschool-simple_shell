ecb513279494967de9e18ef87f7eb980c7543f4f
rubakhalid <rubak.alhumaid@gmail.com>
2025-04-14 13:54:50 +0000

    add task

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define PROMPT "#cisfun$ "
+
+extern char **environ;
 
 /**
  * main - Entry point for simple shell that executes single-word commands
@@ -6,50 +16,56 @@
  */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	pid_t pid;
-	char *argv[2];
+char *line = NULL, *cmd;
+size_t len = 0;
+ssize_t read;
+pid_t pid;
+char *argv[2];
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
+while (1)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
 
-		read = getline(&line, &len, stdin);
-		if (read == -1)
-		{
-			free(line);
-			exit(0); /* Handle Ctrl+D */
-		}
-
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0'; /* Remove newline */
-
-		while (*line == ' ')
-			line++;
-
-		pid = fork();
-		if (pid == 0)
-		{
-			argv[0] = line;
-			argv[1] = NULL;
-
-			if (execve(argv[0], argv, environ) == -1)
-				perror("./hsh");
-			exit(1);
-		}
-		else if (pid > 0)
-		{
-			wait(NULL);
-		}
-		else
-		{
-			perror("fork");
-		}
-	}
-
-	free(line);
-	return (0);
+read = getline(&line, &len, stdin);
+if (read == -1)
+{
+free(line);
+exit(0);
 }
+
+if (line[read - 1] == '\n')
+line[read - 1] = '\0';
+
+cmd = line;
+while (*cmd == ' ')
+cmd++;
+
+if (*cmd == '\0')
+continue;
+
+pid = fork();
+if (pid == 0)
+{
+argv[0] = cmd;
+argv[1] = NULL;
+
+if (execve(argv[0], argv, environ) == -1)
+perror("./hsh");
+
+exit(1);
+}
+else if (pid > 0)
+{
+wait(NULL);
+}
+else
+{
+perror("fork");
+}
+}
+
+free(line);
+return (0);
+}
+
