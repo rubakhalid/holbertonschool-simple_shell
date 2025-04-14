10f0def2a0b7f7b7d6608fe44e805adb3740ce57
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-04-14 08:59:21 +0000

    Split shell code into multiple files - Betty compliant

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <ctype.h>
+#include <string.h>
 
 /**
  * main - Entry point for simple shell
@@ -25,12 +27,38 @@ int main(void)
 		if (line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
-		while (*line == ' ')
-			line++;
+		line = trim_spaces(line);
+		if (*line == '\0')
+			continue;
 
 		execute_command(line);
 	}
 
 	free(line);
 	return (0);
+}
+
+
+/**
+ * trim_spaces - Removes leading and trailing spaces from a string
+ * @str: The input string
+ * Return: Pointer to trimmed string
+ */
+char *trim_spaces(char *str)
+{
+	char *end;
+
+	while (isspace((unsigned char)*str))
+		str++;
+
+	if (*str == 0)
+		return (str);
+
+	end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end))
+		end--;
+
+	end[1] = '\0';
+
+	return (str);
 }
