b32af7e8556319d1f4b16457bda946bd8746ddcb
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-04-14 09:15:27 +0000

    Split shell code into multiple files - Betty compliant

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include "main.h"
-#include <ctype.h>
 #include <string.h>
+
+/**
+ * is_space - Checks if a character is a space or tab
+ * @c: The character to check
+ * Return: 1 if space or tab, 0 otherwise
+ */
+int is_space(char c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+/**
+ * trim_spaces - Removes leading and trailing spaces from a string
+ * @str: The input string
+ * Return: Pointer to trimmed string
+ */
+char *trim_spaces(char *str)
+{
+	char *end;
+
+	while (is_space(*str))
+		str++;
+
+	if (*str == '\0')
+		return (str);
+
+	end = str + strlen(str) - 1;
+	while (end > str && is_space(*end))
+		end--;
+
+	end[1] = '\0';
+
+	return (str);
+}
 
 /**
  * main - Entry point for simple shell
@@ -36,29 +69,4 @@ int main(void)
 
 	free(line);
 	return (0);
-}
-
-
-/**
- * trim_spaces - Removes leading and trailing spaces from a string
- * @str: The input string
- * Return: Pointer to trimmed string
- */
-char *trim_spaces(char *str)
-{
-	char *end;
-
-	while (isspace((unsigned char)*str))
-		str++;
-
-	if (*str == 0)
-		return (str);
-
-	end = str + strlen(str) - 1;
-	while (end > str && isspace((unsigned char)*end))
-		end--;
-
-	end[1] = '\0';
-
-	return (str);
 }
