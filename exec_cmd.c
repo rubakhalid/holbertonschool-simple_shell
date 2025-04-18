cbd9d0daf8a9bf2f34e94113b44f38d27354d566
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-04-18 00:44:41 +0000

    Fix exec_cmd.c: added command path validation and proper error handling

--- exec_cmd.c
+++ exec_cmd.c
@@ -1,46 +1,47 @@
-
 #include "main.h"
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
 
 /**
- * execute_command - Executes a command with optional arguments
- * @line: Command line input
+ * execute_command - Executes a command entered by the user
+ * @cmd_path: path to the command (can be full or relative)
+ * @args: unused
+ * @line: full command line input
  */
-void execute_command(char *line)
+void execute_command(char *cmd_path, char **args, char *line)
 {
-        pid_t pid;
-        char *argv[64];
-        int i = 0;
-        char *token;
+	pid_t pid;
+	char *argv[64];
 
-        token = strtok(line, "  ");
-        while (token != NULL && i < 63)
-        {
-                argv[i++] = token;
-                token = strtok(NULL, "  ");
-        }
-        argv[i] = NULL;
+	parse_line(line, argv);
 
-        if (argv[0] == NULL)
-                return;
+	if (argv[0] == NULL)
+		return;
 
-        pid = fork();
-        if (pid == 0)
-        {
-                if (execve(argv[0], argv, environ) == -1)
-                        perror("hsh");
-                exit(1);
-        }
-        else if (pid > 0)
-        {
-                wait(NULL);
-        }
-        else
-        {
-                perror("fork");
-        }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+
+	if (pid == 0)
+	{
+		if (access(argv[0], X_OK) == 0)
+		{
+			execve(argv[0], argv, environ);
+			perror("execve");
+			exit(1);
+		}
+		else
+		{
+			dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", argv[0]);
+			exit(127);
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
 }
