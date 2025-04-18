8bb7bc40eea2d99716c5d494f74371d8767a5050
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-04-18 01:00:45 +0000

    essential headers and organized prototypes

--- main.h
+++ main.h
@@ -4,17 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <stddef.h>
 
 extern char **environ;
 
+/* Function Prototypes */
+int main(void)
 char *read_line(void);
 void execute_command(char *line);
 void print_prompt(void);
+char *_getenv(const char *name);
+char *find_command_path(char *command);
+char **parse_line(char *line);
+int is_only_whitespace(const char *s);
 void execute_command(char *line);
 char *trim_spaces(char *str);
 int is_space(char c);
+void parse_line(char *line, char **argv);
+void execute_command(char *cmd_path, char **args, char *line);
 
 #endif /* MAIN_H */
