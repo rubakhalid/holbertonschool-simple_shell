c5015f08486ce14c56f6d6af6157f4300b4393dd
rubakhalid <rubak.alhumaid@gmail.com>
2025-04-21 14:22:08 +0000

    Add shell.h - header file with function prototypes and constants

--- shell.h
+++ shell.h
@@ -8,10 +8,15 @@
 #include <sys/wait.h>
 
 int has_path_env(void);
+
 char **parse_line(char *line);
+
 int execute_cmd(char **args);
+
 extern char **environ;
+
 char *find_command(char *command);
+
 extern int last_status;
 
 #endif
